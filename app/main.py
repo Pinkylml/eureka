fa4f80c630839f91878630ef7eb46dcf5909310d
Jefferson <jeff_can1995@hotmail.es>
2024-06-16 22:48:17 -0500

    calculate recall

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from .modules.load_data import load_data_to_dataframe
 from .modules.Corpus import Corpus
 from .modules.retrieval_relevant_docs import retrieval_relevant_docs
+from .modules.evaluation import evaluate
 
 app = FastAPI()
 #df_bow=pd.read_csv('Back_of_words_version2.1.csv', index_col=0)
@@ -25,12 +26,15 @@ def pross_quer(data: Dict[str, str]):
     return {"best_titles_jaccard": best_titles_jacard,
             "best_titles_cos": best_titles_coss}"""
 df=load_data_to_dataframe()
+all_files=df['filename'].tolist()
 corpur=Corpus(df)
 
 @app.get("/{query}")
 async def read_item(query: str):
         corpur.jaccard(query=query)
         corpur.cosine(query=query)
+        recall=evaluate(query,all_files,corpur.best_titles_jaccard)
+        print("Recall jacc: ", recall)
         result=retrieval_relevant_docs(
             df,
             corpur.sorted_indices_jacc,
@@ -38,5 +42,6 @@ async def read_item(query: str):
             corpur.sorted_indices_cos,
             corpur.cosine_distances
             )
+        
         del corpur.sorted_indices_jacc,corpur.jaccard_similarities,corpur.sorted_indices_cos,corpur.cosine_distances
         return result
